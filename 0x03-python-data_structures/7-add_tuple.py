dcd15ea4c41084edb2ecc784af45b7d52e435019
funk-oz <kieranmarvin70@gmail.com>
2022-09-26 15:01:47 +0000

    tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
 
 
+
 def validate_tuple(_tuple=()):
     if len(_tuple) < 2:
         if len(_tuple) == 1:
@@ -15,4 +16,4 @@ def validate_tuple(_tuple=()):
     elif len(_tuple) > 2:
         _tuple = (_tuple[0], _tuple[1])
 
-    return _tuple  
+    return _tuple
