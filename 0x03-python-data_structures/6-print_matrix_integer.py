9ee1c2491d5c8720c28c9566e9b26b9ac95e20e0
funk-oz <kieranmarvin70@gmail.com>
2022-09-26 14:52:20 +0000

    psycode task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for elems in matrix:
+            i = 1
+            length = len(elems)
 
- def print_matrix_integer(matrix=[[]]): 
-     """ 
-     A function that prints a matrix of integers. 
-     """ 
-     for i in range(len(matrix)): 
-         for j in range(len(matrix[i])): 
-             print("{:d}".format(matrix[i][j]), end="") 
-             if j != (len(matrix[i]) - 1): 
-                 print(" ", end="") 
-  
-         print("")
+            for elem in elems:
+                if i == length:
+                    print('{:d}'.format(elem), end='')
+                else:
+                    print('{:d}'.format(elem), end=' ')
+                i += 1
+
+            print()
