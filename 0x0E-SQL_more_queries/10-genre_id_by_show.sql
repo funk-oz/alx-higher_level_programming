0e122dabd916d9870d7c3779ac7155e3f04811c5
funk-oz <kieranmarvin70@gmail.com>
2022-11-30 18:33:33 +0000

    add advanced files

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,4 +5,4 @@ SELECT tvs.title, it.genre_id
 FROM  tv_show_genres it
 INNER JOIN tv_shows tvs
 ON it.show_id = tvs.id
-ORDER BY tvs.title, it.genre_id ASC;iOA
+ORDER BY tvs.title, it.genre_id ASC;
